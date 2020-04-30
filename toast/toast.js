9f68c05c0f418a36866a25c1e621e5dffc5ca4f9
Bonze255 <fragger255@hotmail.com>
2020-04-30 20:38:39 +0200

    update both files
    - now 3 templates can choosen, 
    - error template is implement a button to close the toast and change a item.

--- toast/toast.js
+++ toast/toast.js
@@ -4,69 +4,112 @@ $.widget("sv.status_toast", $.sv.widget, {
 	initSelector: 'span[data-widget="status.toast"]',
 
 	options: {
-		bgcolor: null,
+		bgColor: null,
 		color: null,
 		align: null,
 		position: null,
 		stack: null,
-		hideafter: null,
+		hideAfter: 50000,
 		allowClose: null,
-		showhide: null,
-		showloader: null,
+		showHide: null,
+		showLoader: null,
+		template: null,
 	},
 
 	_create: function() {
 	this._super();
+
 	},
 	
 	_update: function(response) {
 		var id = this.element.attr('id');
-		//response[0-3] = {{item_trigger}}{{item_head}}, {{item_text}}, {{item_icon|default('')}}
-		console.log("toast response", response[0],response[1], response[2], response[3]);
-		console.log("toast options", this.options.bgcolor);
-		
-		var text = '';
-		var head = '';
-		var icon = '';
-		var showHideTrans ='';
-		var allowToastClose = '';
-		var head = '';
-		var icon = '';
-		var showhide ='';
-		var position ='';
-		var textAlign ='';
-		var loaderbg = '';
-		
 		var date = new Date;
+		var timestamp = Date.now();
 		var heute = date.getHours() +":" + date.getMinutes()+":" + date.getSeconds()+" " +date.getDate()+ "."+ (date.getMonth()+ 1)+"." + date.getFullYear()+" <br>";
+		var color;
+		var showIcon;
+		var showTitle;
+		var showText;
+		var allowToastClose;
+		var hideAfter;
+		var showPosition;
+		var showLoader;
+		var textAlign;
+		var loaderBg;
+		var bgColor;
+		var allowClose;
+		var send;
 		
+		//check ob item oder text angegeben
+		//response[0] = triggeritem
+		showTitle = response[1] || this.element.attr('data-info').explode()[0];
+		showText = response[2] || this.element.attr('data-info').explode()[1];
+		send = response[4];
+		
+		if(this.options.template == 'info'){
+			console.log("template info");
+			showIcon = 'info';
+			hideAfter = 50000;
+			bgColor = 'gray';
+			color = '#eee';
+		}else if (this.options.template == 'warning'){
+			console.log("template warning");
+			showIcon = 'warning';
+			hideAfter = self.options.hideAfter;
+			bgColor = '#ff6609';
+			color = '#000000';
+		}else if (this.options.template == 'error'){
+			console.log("template error");
+			showIcon = 'error';
+			hideAfter= false;
+			bgColor = '#e03d3d';
+			allowClose = false;
+			color = '#FFF';
+			showText+='<br/><input class ="button ui-btn ui-mini ui-corner-all ui-btn-inline"  id ='+timestamp+' type="button" value="'+this.element.attr('data-button').explode()[0]+'" />';
+		}else{
+			console.log("template free");
+			showIcon = response[3] || this.element.attr('data-info').explode()[2];
+			hideAfter = self.options.hideAfter;
+			bgColor = self.options.bgColor;
+			allowClose = self.options.allowClose;
+			color= self.options.color;
+		};
 		//if (response[0] == 1){
-			var iconfs = $('#icon').text();
-			var textfs = $('#text').text();
-			var titlefs = $('#title').text();
-			$('#' + 'icon').remove();
-			$('#' + 'text').remove();
-			$('#' + 'title').remove();
-			console.log("icon", iconfs);
+			
+			console.log("title, text, icon", showTitle, showText, showIcon);
 			var toast = $.toast({
-				text: heute+response[2], // Text that is to be shown in the toast
-				heading: titlefs+response[1], // Optional heading to be shown on the toast
-				icon: response[3], // Type of toast icon
-				showHideTransition: this.options.showhide, // fade, slide or plain
-				allowToastClose: this.options.allowclose, // Boolean value true or false
-				hideAfter: this.options.hideafter, // false to make it sticky or number representing the miliseconds as time after which toast needs to be hidden
+				text: showText, // Text that is to be shown in the toast
+				heading: showTitle, // Optional heading to be shown on the toast
+				icon: showIcon, // Type of toast icon
+				showHideTransition: this.element.attr('data-showHide'), // fade, slide or plain
+				allowToastClose: allowClose, // Boolean value true or false
+				hideAfter: hideAfter, // false to make it sticky or number representing the miliseconds as time after which toast needs to be hidden
 				stack: this.options.stack, // false if there should be only one toast at a time or a number representing the maximum number of toasts to be shown at a time
 				position: this.options.position, // bottom-left or bottom-right or bottom-center or top-left or top-right or top-center or mid-center or an object representing the left, right, top, bottom values
 				textAlign: this.options.align,  // Text alignment i.e. left, right or center
-				loader: this.options.showloader,  // Whether to show loader or not. True by default
-				loaderBg: this.options.loaderbg,  // Background color of the toast loader
-				bgColor: this.options.bgcolor,
-				textColor: this.options.color
+				loader: this.options.showLoader,  // Whether to show loader or not. True by default
+				loaderBg: this.options.loaderBg,  // Background color of the toast loader
+				bgColor: bgColor,
+				textColor: color
+				
 			});
-		//}//else{
-		//	toast.reset(); // remove the toast "Some toast that needs to be removed"
-		//}
+		
 		$('#' + id).append(toast);
-	},
+	
 
+	$(".button").click(function() {
+		var id = $(this).attr('id');
+		console.log("button pressed "+ id);
+		if (send != undefined){
+			io.write(send, 'True');
+		};
+		
+		console.log($(this).parent().get( 0 ));
+		$(this).parent().get( 0 ).remove();
+	});
+	
+	},
+	
+	_events: {
+	}
 });
