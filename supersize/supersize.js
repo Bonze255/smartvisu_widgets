086e68723bab90886158d4e10a8ec49ff5861dd2
Bonze255 <fragger255@hotmail.com>
2021-01-21 09:33:10 +0100

    Add files via upload

--- supersize/supersize.js
+++ supersize/supersize.js
@@ -17,14 +17,20 @@ $.widget("sv.supersize", $.sv.widget, {
 		var supersized = false;
 		$(".supersize").click(function () {
 			if (supersized == false){
-				$(this).closest("div .block").css( "width", "100%" );
-				$(this).closest("div .block").css( "display", "block" );
+				$(this).closest("div .block").addClass("overlay");
+				$('.overlay').show('slow');
+				//var content = $(this).closest("div .block").text();
+				//$(this).closest("div .block").addClass('overlay');
+
+				//$(this).closest("div .block").css( "width", "100%" );
+				//$(this).closest("div .block").css( "display", "block" );
 				console.log("SUPERSIZE ME");
 				supersized = true;
-				$(this).closest("supersize").attr("src", "icons/ws/control_arrow_down_left.svg");
+				$(this).closest(".supersize").attr("src", "icons/ws/control_arrow_down_left.svg");
 			}else{
-				$(this).closest("supersize").attr("src", "icons/ws/control_arrow_up_right.svg");
-				$(this).closest("div .block").css( "width", "50%" );
+				$(this).closest("div .block").removeClass("overlay");
+				$(this).closest(".supersize").attr("src", "icons/ws/control_arrow_up_right.svg");
+				
 				console.log("MINIMIZE ME");
 				supersized = false;
 			}
