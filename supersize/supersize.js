4cf7b125ec8e47ac576e255823079dfe2efeea82
Bonze255 <fragger255@hotmail.com>
2021-01-28 13:21:06 +0100

    Add files via upload
    add black overlay mask

--- supersize/supersize.js
+++ supersize/supersize.js
@@ -3,36 +3,40 @@ $.widget("sv.supersize", $.sv.widget, {
 
 	initSelector: '[data-widget="supersize"]',
 
-	options: {
+	options:{
+
 	},
 
 	_create: function () {
 		this._super();
 		this.element.cycle();
+		$("#index").append('<div class="mask"> </div>');
 	},
 
 	_update: function (response) {
 		var supersized = false;
-		$(".supersize").click(function (e) {
-
-			if (supersized == false){
-				$(this).closest("div").addClass("overlay");
-				$('.overlay').show('slow');
-				$(this).closest("div").css( "width", "100%" );
-				$(this).closest(".supersize").attr("src", "\icons/sw/jquery_minus.svg");
-				console.log("SUPERSIZE ME");
-				supersized = true;
-			}else{
-				$(this).closest("div").removeClass("overlay");
-				console.log("MINIMIZE ME");
-				supersized = false;
-				$(this).closest(".supersize").attr("src", "\icons/sw/control_arrow_up_right.svg");
-			}
-			
-			e.stopPropagation();
-			e.stopImmediatePropagation();
+			$(".supersize").click(function (e) {
+				if (supersized == false){
+					$(".mask").css('display','block');
+					$(this).closest("div").addClass("overlay");
+					$('.overlay').show('slow');
+					$(this).closest("div").css( "width", "100%" );
+					$(this).closest(".supersize").attr("src", "\icons/sw/jquery_minus.svg");
+					console.log("SUPERSIZE ME");
+					supersized = true;
+				}else{
+					$(".mask").css('display','none');
+					$(this).closest("div").removeClass("overlay");
+					console.log("MINIMIZE ME");
+					supersized = false;
+					$(this).closest(".supersize").attr("src", "\icons/sw/control_arrow_up_right.svg");
+				}
+			//$(window).trigger('resize');
+		    e.stopPropagation();
+			e.stopImmediatePropagation();     
 		});
-	},
+},
+
 
 	_repeat: function () {
 
@@ -42,3 +46,6 @@ $.widget("sv.supersize", $.sv.widget, {
 	}
 
 });
+
+
+
